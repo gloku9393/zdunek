4e45eee9fea88dcc221e57f5787fc8b71011b07d
Mateusz Torbiarczyk <200363@student.pwr.wroc.pl>
2017-06-06 23:33:42 +0200

    Refactor, wyznaczanie statystyk

--- pcaB.m
+++ pcaB.m
@@ -1,21 +1,21 @@
 function [V pc D] = pcaB(data, J)
     data = data';
-    hold on;
-    plot(data(:,1), data(:,2), 'o');
+  %  hold on;
+  %  plot(data(:,1), data(:,2), 'o');
     [n, m] = size(data);
     dMean = mean(data);
     dStd = std(data);
     B = (data - repmat(dMean,[n 1])) ./ repmat(dStd,[n 1]);
     C = cov(B);
     [V D] = eigs(C,J);
-    [D order] = sort(diag(D), 'ordered');
+    [D order] = sort(diag(D), 'descend');
     V = V(:, order);
     x1=linspace(0,V(1,1));
     y1=linspace(0,V(1,2));
     x2=linspace(0,V(2,1));
     y2=linspace(0,V(2,2));
-    plot(x1,y1);
-    plot(x2,y2);
+ %   plot(x1,y1);
+ %   plot(x2,y2);
     pc=data*V; %uwaga moze zle 
-    plot(p(:,1), p(:,2), 'ro');
+ %   plot(p(:,1), p(:,2), 'ro');
 end
